4e686204956383664de4fa8868802b581d2e8e02
tharunkumar5802 <114410793+tharunkumar5802@users.noreply.github.com>
2026-09-22 09:56:09 +0530

    sample task during class

---
+++ sample_task/script.js
@@ -0,0 +1,6 @@
+var heading=document.getElementById('head');
+var btn=document.getElementById('click');
+
+btn.addEventListener('click',function(){
+    heading.textContent='hai this is a sample project'
+})
